6be18e0ce4f84df36f300ecbd394415843a76b2d
EliasGrafstrom <elias@supershow.se>
2025-04-11 15:02:50 +0200

    Add files via upload

---
+++ Data/Repositories/ProjectRepository.cs
@@ -0,0 +1,36 @@
+﻿using Data.Contexts;
+using Data.Entitites;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Data.Repositories;
+
+public interface IProjectRepository : IBaseRepository<ProjectEntity>
+{
+
+}
+
+public class ProjectRepository(AppDbContext context) : BaseRepository<ProjectEntity>(context), IProjectRepository
+{
+    public override async Task<IEnumerable<ProjectEntity>> GetAllAsync()
+    {
+        var entities = await _table
+            .Include(x => x.User)
+            .Include(x => x.Status)
+            .Include(x => x.Client)
+            .ToListAsync();
+
+        return entities;
+    }
+
+    public override async Task<ProjectEntity?> GetAsync(Expression<Func<ProjectEntity, bool>> expression)
+    {
+        var entity = await _table
+            .Include(x => x.User)
+            .Include(x => x.Status)
+            .Include(x => x.Client)
+            .FirstOrDefaultAsync(expression);
+
+        return entity;
+    }
+}
